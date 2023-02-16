6ca6b6f0368407ec94fe695e6d47fe4f5a707a82
Pylyp Borysov <pborysov@nexuscognitive.com>
2023-02-16 10:56:17 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button @click="hello">Hello</button>
+    <button @click="error">Error</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+function hello(){
+  console.log('Hello');
+}
+function error(){
+  console.log(a);
+}
+</script>
